882a3938e3e663b70b0bb1c6c17db9ce096a2e8a
seekerpig <laserklet@gmail.com>
2022-09-16 12:11:50 +0800

    updateed nmaing
    updated naming

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import Signup from "./Signup";
-import LoginForm from "./Login";
-import Homepage from "./Homepage";
-import Creategame from "./Creategame";
-import Profile from "./Profile";
+import SignupForm from "./SignupForm";
+import LoginForm from "./LoginForm";
+import HomePage from "./HomePage";
+import CreateGameForm from "./CreateGameForm";
+import ProfilePage from "./ProfilePage";
 import {AuthProvider} from "../contexts/AuthContext";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
@@ -20,11 +20,12 @@ function App() {
         <Router>
         <AuthProvider>
           <Routes>
-            <Route exact path="/" element={<Homepage/>}/>
-            <Route path="/signup" element={<Signup/>}/>
+            <Route exact path="/" element={<HomePage/>}/>
+            <Route path="/signup" element={<SignupForm/>}/>
             <Route path="/login" element={<LoginForm/>}/>
-            <Route path="/creategame" element={<Creategame/>}/>
-            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/creategame" element={<CreateGameForm/>}/>
+            <Route path="/profile" element={<ProfilePage/>}/>
+            <Route path="/explore" element={<ExplorePage/>}/>
           </Routes>
           
         </AuthProvider>
---
+++ src/components/DetailedGamePage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DetailedGamePage() {
+  return (
+    <div>DetailedGamePage</div>
+  )
+}
