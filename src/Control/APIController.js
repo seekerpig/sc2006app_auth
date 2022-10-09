e03e3e0198ccb0acfde97e012a90c294faf8dba8
seekerpig <laserklet@gmail.com>
2022-10-09 18:37:28 +0800

    apicontroller

---
+++ src/Control/APIController.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+//this one connect to GovDataAPI, then call database controller to store data
+
+export default function APIController() {
+  return (
+    <div>APIController</div>
+  )
+}
